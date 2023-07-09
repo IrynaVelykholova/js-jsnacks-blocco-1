950a1fff7fc64c6e1440b638f71563b3cbd1eaea
IrynaVelykholova <irynavely93@gmail.com>
2023-07-09 20:35:28 +0200

    sistemato le cartelle

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,10 +2,6 @@
 const nomi = ["Franca", "Franco", "Franchina", "Franchino", "Franchetta"];
 const cognomi = ["Rossi", "Ferrari", "Baudo", "Bianchi", "Colombo"];
 
-const randomNomi = Math.floor(Math.random() * nomi.length);
-const randomCognomi = Math.floor(Math.random() * cognomi.length);
-
-
 for (let i = 0; i < 15; i++) {
     const randomCognomi = Math.floor(Math.random() * cognomi.length);
     const randomNomi = Math.floor(Math.random() * nomi.length);
